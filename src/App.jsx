9b9738bf48e83e0cf6b84b81aba2767b71a17b41
unknown <fsowah001@gmail.com>
2025-08-05 08:57:33 +0000

    project setup with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
